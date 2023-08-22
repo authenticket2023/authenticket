b496149c6738defd0282ff601513bbb4b1f218dc
petrinawjt <petrinawong1909@gmail.com>
2023-08-22 19:26:07 +0800

    frontend clean up

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,7 +7,6 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { createRoot } from 'react-dom/client';
 //for normal user
-import { Health } from './pages/Health';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { Page404 } from './pages/Page404';
@@ -16,10 +15,6 @@ import { About } from './pages/About';
 import { Signup } from './pages/Signup';
 import { Forbidden } from './pages/Forbidden';
 //for admin user
-import { Elderly_admin } from './pages/Elderly(admin)';
-import { Home_admin } from './pages/Home(admin)';
-import { Post_admin } from './pages/Post(admin)';
-import { Record_admin } from './pages/Record(admin)';
 
 
 
@@ -28,7 +23,6 @@ const root = createRoot(container);
 root.render(
 	<BrowserRouter>
 		<Routes>
-			<Route path="/Health" element={<Health />} />
 			<Route path="/Home" element={<Home />} />
 			<Route path="/" element={<Navigate to="/Home" />} />
 			<Route path="/Login" element={<Login />} />
@@ -38,10 +32,6 @@ root.render(
 			<Route path="/Signup" element={<Signup />} />
 			<Route path="/Forbidden" element={<Forbidden />} />
 			<Route path="/*" element={<Page404 />} />
-			<Route path="/Home-admin" element={<Home_admin />} />
-			<Route path="/Elderly-admin" element={<Elderly_admin />} />
-			<Route path="/Post-admin" element={<Post_admin />} />
-			<Route path="/Record-admin" element={<Record_admin/>} />
 		</Routes>
 	</BrowserRouter>);
 
