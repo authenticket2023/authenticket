f9caf1a23de17f86ee70d0eb01983814ef56f47d
xXxPMWxXx <panmingwei123@gmail.com>
2023-08-31 15:52:28 +0800

    organiser/admin navbar and routing

---
+++ frontend/src/pages/EventOrganiser/EventOrganiser.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { NavbarOrganiser } from '../../Navbar';
+
+export const EventOrganiser = () => {
+    useEffect(() => {
+    }, []);
+
+    return (
+        <div>
+            < NavbarOrganiser />
+            <h1>
+                EventOrganiser Page under construction ...
+            </h1>
+        </div>
+
+    )
+}
