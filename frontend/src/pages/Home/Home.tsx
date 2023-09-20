51ef77baef15958cfa5d0d759e898dd1fe984b6f
nafutofu <naufal.syaqil@gmail.com>
2023-09-20 12:19:24 +0800

    bestSellers
    able to get carousel to work with backend api, need to debug images, after that can reapply code to the others quite quickly

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { NavbarNotLoggedIn } from "../../Navbar";
 import { styled, alpha } from "@mui/material/styles";
+import CardMedia from '@mui/material/CardMedia'
+import Card from '@mui/material/Card';
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
@@ -17,12 +19,15 @@ import BearCarousel, {
   BearSlideCard,
 } from "bear-react-carousel";
 import { async } from "q";
+import { CardActionArea } from "@mui/material";
 
 export const Home = () => {
-  
+
   const [featured, setFeatured]: any = React.useState();
+  const [bestSellers, setBestSellers]: any = React.useState();
   const [loaded, setLoaded]: any = React.useState(false);
-  
+  const [bsLoaded, setBSLoaded]: any = React.useState(false);
+
   const loadFeatured = async () => {
     // //calling backend API
     fetch(`${process.env.REACT_APP_BACKEND_DEV_URL}/public/event/featured`, {
@@ -39,84 +44,35 @@ export const Home = () => {
           setLoaded(true);
         } else {
           //passing to parent component
-          //show alert msg
-          // props.setOpenSnackbar(true);
-          // props.setAlertType('error');
-          // props.setAlertMsg(`Fetch data failed, code: ${response.status}`);
         }
       })
       .catch((err) => {
         window.alert(err);
       });
-    }
+  }
 
-  const images = [
-    {
-      id: 1, bg: "https://picsum.photos/400/250",
-      subheader: "Featured",
-      header: "Lauv: the between albums tour",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta\
-    earum ullam repellat minus, cumque, tempore fugit officia delectus\
-    omnis numquam dolor adipisci ut voluptatem dolore odit cum sunt?\
-    Lorem ipsum dolor sit amet consectetur adipisicing elit.\
-    Architecto maiores reprehenderit veritatis eveniet possimus ex\
-    odio? Reiciendis, ducimus. Placeat recusandae nulla nisi vitae est\
-    quod libero sint? Laborum, at maiores.",
-      link: "#"
-    },
-    {
-      id: 2, bg: "https://picsum.photos/400/250",
-      subheader: "Featured",
-      header: "Lauv: the between albums tour",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta\
-    earum ullam repellat minus, cumque, tempore fugit officia delectus\
-    omnis numquam dolor adipisci ut voluptatem dolore odit cum sunt?\
-    Lorem ipsum dolor sit amet consectetur adipisicing elit.\
-    Architecto maiores reprehenderit veritatis eveniet possimus ex\
-    odio? Reiciendis, ducimus. Placeat recusandae nulla nisi vitae est\
-    quod libero sint? Laborum, at maiores.",
-      link: "#"
-    },
-    {
-      id: 3, bg: "https://picsum.photos/400/250",
-      subheader: "Featured",
-      header: "Lauv: the between albums tour",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta\
-    earum ullam repellat minus, cumque, tempore fugit officia delectus\
-    omnis numquam dolor adipisci ut voluptatem dolore odit cum sunt?\
-    Lorem ipsum dolor sit amet consectetur adipisicing elit.\
-    Architecto maiores reprehenderit veritatis eveniet possimus ex\
-    odio? Reiciendis, ducimus. Placeat recusandae nulla nisi vitae est\
-    quod libero sint? Laborum, at maiores.",
-      link: "#"
-    },
-    {
-      id: 4, bg: "https://picsum.photos/400/250",
-      subheader: "Featured",
-      header: "Lauv: the between albums tour",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta\
-    earum ullam repellat minus, cumque, tempore fugit officia delectus\
-    omnis numquam dolor adipisci ut voluptatem dolore odit cum sunt?\
-    Lorem ipsum dolor sit amet consectetur adipisicing elit.\
-    Architecto maiores reprehenderit veritatis eveniet possimus ex\
-    odio? Reiciendis, ducimus. Placeat recusandae nulla nisi vitae est\
-    quod libero sint? Laborum, at maiores.",
-    link: "#"
-    },
-    {
-      id: 5, bg: "https://picsum.photos/400/250",
-      subheader: "Featured",
-      header: "Lauv: the between albums tour",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta\
-    earum ullam repellat minus, cumque, tempore fugit officia delectus\
-    omnis numquam dolor adipisci ut voluptatem dolore odit cum sunt?\
-    Lorem ipsum dolor sit amet consectetur adipisicing elit.\
-    Architecto maiores reprehenderit veritatis eveniet possimus ex\
-    odio? Reiciendis, ducimus. Placeat recusandae nulla nisi vitae est\
-    quod libero sint? Laborum, at maiores.",
-      link: "#"
-    },
-  ];
+  const loadBestSellers = async () => {
+    // //calling backend API
+    fetch(`${process.env.REACT_APP_BACKEND_DEV_URL}/public/event/bestseller`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'GET',
+    })
+      .then(async (response) => {
+        if (response.status == 200) {
+          const apiResponse = await response.json();
+          const data = apiResponse.data;
+          setBestSellers(data);
+          setBSLoaded(true);
+        } else {
+          //passing to parent component
+        }
+      })
+      .catch((err) => {
+        window.alert(err);
+      });
+  }
 
   const CustomBanner = () => {
     const bearSlideItemData: TBearSlideItemDataList = featured.map((row: any) => {
@@ -138,7 +94,7 @@ export const Home = () => {
       />
     );
   };
-  
+
   const TextAnimationsCarousel = () => {
     const slideItemData: TBearSlideItemDataList = featured.map((row: any) => {
       return {
@@ -159,7 +115,7 @@ export const Home = () => {
                 variant="outlined"
                 href='#'
                 sx={{ color: "white", borderColor: "white" }}
-                >
+              >
                 Get tickets
               </Button>
             </Box>
@@ -212,219 +168,213 @@ export const Home = () => {
     },
   }));
 
-  function TicketCarousel() {
+  function BestSellerItem(props: any) {
     return (
-      <Carousel
-        additionalTransfrom={0}
-        arrows
-        autoPlaySpeed={3000}
-        centerMode={false}
-        className=""
-        containerClass="container"
-        dotListClass=""
-        draggable
-        focusOnSelect={false}
-        infinite
-        itemClass=""
-        keyBoardControl
-        minimumTouchDrag={80}
-        partialVisible
-        pauseOnHover
-        renderArrowsWhenDisabled={false}
-        renderButtonGroupOutside={false}
-        renderDotsOutside={false}
-        responsive={{
-          desktop: {
-            breakpoint: {
-              max: 3000,
-              min: 1024,
-            },
-            items: 3,
-            partialVisibilityGutter: 20,
-          },
-          mobile: {
-            breakpoint: {
-              max: 464,
-              min: 0,
-            },
-            items: 1,
-            partialVisibilityGutter: 30,
-          },
-          tablet: {
-            breakpoint: {
-              max: 1024,
-              min: 464,
-            },
-            items: 2,
-            partialVisibilityGutter: 30,
-          },
-        }}
-        rewind={false}
-        rewindWithAnimation={false}
-        rtl={false}
-        shouldResetAutoplay
-        showDots={false}
-        sliderClass=""
-        slidesToSlide={1}
-        swipeable
-      >
-        <Box>
-          <Grid container>
-            <Grid item>
-              <img src="https://picsum.photos/400/250" />
-            </Grid>
-          </Grid>
-        </Box>
-        <Box>
-          <Grid container marginRight={5}>
-            <Grid item>
-              <img src="https://picsum.photos/400/250" />
-            </Grid>
-          </Grid>
-        </Box>
-        <Box>
-          <Grid container marginRight={5}>
-            <Grid item>
-              <img src="https://picsum.photos/400/250" />
-            </Grid>
-          </Grid>
-        </Box>
-        <Box>
-          <Grid container marginRight={5}>
-            <Grid item>
-              <img src="https://picsum.photos/400/250" />
-            </Grid>
-          </Grid>
-        </Box>
-      </Carousel>
-    );
+      <Box marginTop = {2} marginRight={1}>
+        <Card sx={{minHeight: 175}}>
+          <CardMedia image={`https://authenticket.s3.ap-southeast-1.amazonaws.com/event_images/${props.eventImage}`}>
+          </CardMedia>
+          <CardActionArea href='#'>
+          </CardActionArea>
+        </Card>
+        <Typography>
+          {props.eventName}
+        </Typography>
+      </Box>
+    )
   }
 
+  function BestSellersCarousell() {
+    const bestSellerListItems = bestSellers.map((event: any) => {
+      return <BestSellerItem eventName={event.eventName} eventImage={event.eventImage}></BestSellerItem>
+    });
+    return <Carousel
+      additionalTransfrom={0}
+      arrows
+      autoPlaySpeed={3000}
+      centerMode={false}
+      className=""
+      containerClass="container"
+      dotListClass=""
+      draggable
+      focusOnSelect={false}
+      infinite
+      itemClass=""
+      keyBoardControl
+      minimumTouchDrag={80}
+      partialVisible
+      pauseOnHover
+      renderArrowsWhenDisabled={false}
+      renderButtonGroupOutside={false}
+      renderDotsOutside={false}
+      responsive={{
+        desktop: {
+          breakpoint: {
+            max: 3000,
+            min: 1024,
+          },
+          items: 3,
+          partialVisibilityGutter: 20,
+        },
+        mobile: {
+          breakpoint: {
+            max: 464,
+            min: 0,
+          },
+          items: 1,
+          partialVisibilityGutter: 30,
+        },
+        tablet: {
+          breakpoint: {
+            max: 1024,
+            min: 464,
+          },
+          items: 2,
+          partialVisibilityGutter: 30,
+        },
+      }}
+      rewind={false}
+      rewindWithAnimation={false}
+      rtl={false}
+      shouldResetAutoplay
+      showDots={false}
+      sliderClass=""
+      slidesToSlide={1}
+      swipeable
+    >
+      {bestSellerListItems}
+    </Carousel>;
+  }
+
+
+
+
   useEffect(() => {
-    if (!loaded) {
-        loadFeatured();
+    if (!loaded || !bsLoaded) {
+      loadFeatured();
+      loadBestSellers();
     }
-}, []);
+  }, []);
 
   return (
     <>
-      {loaded ? 
-      <Box>
-        <div>
-          <NavbarNotLoggedIn />
-          <Paper
-            elevation={5}
-            sx={{
-              position: "relative",
-              backgroundColor: "grey.800",
-              color: "#fff",
-              mb: 4,
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-              backgroundImage: `url(https://i.imgur.com/UKi8jbp.png)`,
-            }}
-          >
-            <Box
+      {loaded ?
+        <Box>
+          <div>
+            <NavbarNotLoggedIn />
+            <Paper
+              elevation={5}
               sx={{
                 position: "relative",
-                top: 0,
-                bottom: 0,
-                right: 0,
-                left: 0,
-                backgroundColor: "rgba(0,0,0,.3)",
+                backgroundColor: "grey.800",
+                color: "#fff",
+                mb: 4,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundImage: `url(https://i.imgur.com/UKi8jbp.png)`,
               }}
-            />
-            <Grid
-              container
-              spacing={4}
-              alignItems="center"
-              justifyContent="center"
             >
-              <Grid item md={6}>
-                <Box
-                  sx={{
-                    position: "relative",
-                    p: { xs: 3, md: 6 },
-                    pr: { md: 0 },
-                  }}
-                >
-                  <br />
-                  <br />
-                  <Typography
-                    component="h1"
-                    variant="h3"
-                    color="inherit"
-                    align="center"
+              <Box
+                sx={{
+                  position: "relative",
+                  top: 0,
+                  bottom: 0,
+                  right: 0,
+                  left: 0,
+                  backgroundColor: "rgba(0,0,0,.3)",
+                }}
+              />
+              <Grid
+                container
+                spacing={4}
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Grid item md={6}>
+                  <Box
+                    sx={{
+                      position: "relative",
+                      p: { xs: 3, md: 6 },
+                      pr: { md: 0 },
+                    }}
                   >
-                    Unlock Unforgettable Experiences
-                  </Typography>
-                  <Typography
-                    component="h1"
-                    variant="h6"
-                    color="inherit"
-                    gutterBottom
-                    align="center"
-                  >
-                    your gateway to premier event adventures
-                  </Typography>
-                  <br />
-                  <br />
-                  <br />
-                  <Search>
-                    <SearchIconWrapper>
-                      <SearchIcon sx={{ color: "#3b3b3b" }} />
-                    </SearchIconWrapper>
-                    <StyledInputBase
-                      placeholder="Search…"
-                      inputProps={{ "aria-label": "search" }}
-                      fullWidth
-                    />
-                  </Search>
-                  <br />
-                  <br />
-                  <br />
-                </Box>
+                    <br />
+                    <br />
+                    <Typography
+                      component="h1"
+                      variant="h3"
+                      color="inherit"
+                      align="center"
+                    >
+                      Unlock Unforgettable Experiences
+                    </Typography>
+                    <Typography
+                      component="h1"
+                      variant="h6"
+                      color="inherit"
+                      gutterBottom
+                      align="center"
+                    >
+                      your gateway to premier event adventures
+                    </Typography>
+                    <br />
+                    <br />
+                    <br />
+                    <Search>
+                      <SearchIconWrapper>
+                        <SearchIcon sx={{ color: "#3b3b3b" }} />
+                      </SearchIconWrapper>
+                      <StyledInputBase
+                        placeholder="Search…"
+                        inputProps={{ "aria-label": "search" }}
+                        fullWidth
+                      />
+                    </Search>
+                    <br />
+                    <br />
+                    <br />
+                  </Box>
+                </Grid>
+              </Grid>
+            </Paper>
+          </div>
+          <Typography marginLeft={10} marginTop={8} sx={{ fontWeight: "bold" }}>
+            Best Sellers
+          </Typography>
+          <Grid container>
+            <Grid item xs={12}>
+              <BestSellersCarousell></BestSellersCarousell>
+            </Grid>
+          </Grid>
+          <Typography marginLeft={10} marginTop={8} sx={{ fontWeight: "bold" }}>
+            New on AuthenTicket
+          </Typography>
+          <Grid container>
+            <Grid item xs={12}>
+              <BestSellersCarousell></BestSellersCarousell>
+            </Grid>
+          </Grid>
+          <Box bgcolor="#FF5C35" marginTop={12}>
+            <Grid container alignItems="center" justifyContent="center">
+              <Grid item xs={5} marginTop={4} marginBottom={4}>
+                <CustomBanner></CustomBanner>
+              </Grid>
+              <Grid item xs={5} marginLeft={4}>
+                <TextAnimationsCarousel></TextAnimationsCarousel>
               </Grid>
             </Grid>
-          </Paper>
-        </div>
-        <Typography marginLeft={10} marginTop={8} sx={{ fontWeight: "bold" }}>
-          Featured Events
-        </Typography>
-        <Grid container>
-          <Grid item xs={12}>
-            <TicketCarousel></TicketCarousel>
-          </Grid>
-        </Grid>
-        <Typography marginLeft={10} marginTop={8} sx={{ fontWeight: "bold" }}>
-          New on AuthenTicket
-        </Typography>
-        <Grid container>
-          <Grid item xs={12}>
-            <TicketCarousel></TicketCarousel>
-          </Grid>
-        </Grid>
-        <Box bgcolor="#FF5C35" marginTop={12}>
-          <Grid container alignItems="center" justifyContent="center">
-            <Grid item xs={5} marginTop={4} marginBottom={4}>
-              <CustomBanner></CustomBanner>
-            </Grid>
-            <Grid item xs={5} marginLeft={4}>
-              <TextAnimationsCarousel></TextAnimationsCarousel>
+          </Box>
+          <Typography marginLeft={10} marginTop={8} sx={{ fontWeight: "bold" }}>
+            Recently Added
+          </Typography>
+          <Grid container>
+            <Grid item xs={12} marginBottom={8}>
+              <BestSellersCarousell></BestSellersCarousell>
             </Grid>
           </Grid>
         </Box>
-        <Typography marginLeft={10} marginTop={8} sx={{ fontWeight: "bold" }}>
-          Recently Added
-        </Typography>
-        <Grid container>
-          <Grid item xs={12} marginBottom={8}>
-            <TicketCarousel></TicketCarousel>
-          </Grid>
-        </Grid>
-      </Box>
-        :null}
+        : null}
     </>
   );
 };
