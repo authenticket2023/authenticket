0674c110f00ef883d8ca7b432d4a4a897c72298a
petrinawjt <petrinawong1909@gmail.com>
2023-09-26 15:22:25 +0800

    Revert "Merge pull request #45 from authenticket2023/AUT-22/feature/FE-Homepage"
    This reverts commit 63be79b681b56d77fd3907bd9667927d15fb719f, reversing
    changes made to 549b809bb3806fa0fd8abb455fac8cf3c76ef590.

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { NavbarNotLoggedIn, NavbarLoggedIn } from "../../Navbar";
 import { styled, alpha } from "@mui/material/styles";
-import CardMedia from "@mui/material/CardMedia";
-import Card from "@mui/material/Card";
+import CardMedia from '@mui/material/CardMedia'
+import Card from '@mui/material/Card';
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
@@ -24,7 +24,8 @@ import { CardActionArea } from "@mui/material";
 import { Link } from 'react-router-dom';
 
 export const Home = () => {
-  const token = window.localStorage.getItem("accessToken");
+
+  const token = window.localStorage.getItem('accessToken');
   const [featured, setFeatured]: any = React.useState([]);
   const [bestSellers, setBestSellers]: any = React.useState([]);
   const [recents, setRecents]: any = React.useState([]);
@@ -42,15 +43,12 @@ export const Home = () => {
 
   const loadFeatured = async () => {
     // //calling backend API
-    fetch(
-      `${process.env.REACT_APP_BACKEND_URL}/public/event/featured?page=0&size=3`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "GET",
-      }
-    )
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/public/event/featured?page=0&size=3`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'GET',
+    })
       .then(async (response) => {
         if (response.status == 200) {
           const apiResponse = await response.json();
@@ -81,19 +79,16 @@ export const Home = () => {
       .catch((err) => {
         window.alert(err);
       });
-  };
+  }
 
   const loadBestSellers = async () => {
     // //calling backend API
-    fetch(
-      `${process.env.REACT_APP_BACKEND_URL}/public/event/bestseller?page=0&size=7`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "GET",
-      }
-    )
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/public/event/bestseller?page=0&size=7`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'GET',
+    })
       .then(async (response) => {
         if (response.status == 200) {
           const apiResponse = await response.json();
@@ -108,7 +103,7 @@ export const Home = () => {
             eventDate: bestseller.eventDate,
             totalTickets: bestseller.totalTickets,
             eventLocation: bestseller.eventLocation,
-          }));
+          }))
 
           setBestSellers(bsArr);
           setBSLoaded(true);
@@ -121,15 +116,15 @@ export const Home = () => {
       .catch((err) => {
         window.alert(err);
       });
-  };
+  }
 
   const loadRecents = async () => {
     // //calling backend API
     fetch(`${process.env.REACT_APP_BACKEND_URL}/public/event/recently-added`, {
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      method: "GET",
+      method: 'GET',
     })
       .then(async (response) => {
         if (response.status == 200) {
@@ -144,7 +139,7 @@ export const Home = () => {
             eventDate: recent.eventDate,
             totalTickets: recent.totalTickets,
             eventLocation: recent.eventLocation,
-          }));
+          }))
 
           setRecents(bsArr);
           setRecLoaded(true);
@@ -157,19 +152,16 @@ export const Home = () => {
       .catch((err) => {
         window.alert(err);
       });
-  };
+  }
 
   const loadUpcoming = async () => {
     // //calling backend API
-    fetch(
-      `${process.env.REACT_APP_BACKEND_URL}/public/event/upcoming?page=0&size=10`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "GET",
-      }
-    )
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/public/event/upcoming?page=0&size=10`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'GET',
+    })
       .then(async (response) => {
         if (response.status == 200) {
           const apiResponse = await response.json();
@@ -183,7 +175,7 @@ export const Home = () => {
             eventDate: upcm.eventDate,
             totalTickets: upcm.totalTickets,
             eventLocation: upcm.eventLocation,
-          }));
+          }))
 
           setUpcoming(bsArr);
           setUpcLoaded(true);
@@ -196,85 +188,81 @@ export const Home = () => {
       .catch((err) => {
         window.alert(err);
       });
-  };
+  }
 
   const CustomBanner = () => {
-    const bearSlideItemData: TBearSlideItemDataList = featured.map(
-      (row: any) => {
-        return {
-          key: row.id,
-          children: (
-            <Box bgcolor="#FF5C35">
-              <Grid container justifyContent={"center"}>
-                <Grid item xs={6} bgcolor="#FF5C35">
-                  <BearSlideCard>
-                    <div
-                      style={{
-                        position: "relative",
-                        width: "100%",
-                        height: "400px",
-                      }}
-                    >
-                      <img
-                        src={`https://authenticket.s3.ap-southeast-1.amazonaws.com/event_images/${row.eventImage}`}
-                        style={{
-                          position: "absolute",
-                          top: "50%",
-                          left: "50%",
-                          transform: "translate(-50%, -50%)",
-                          maxWidth: "100%",
-                          maxHeight: "100%",
-                        }}
-                      />
-                    </div>
-                  </BearSlideCard>
-                </Grid>
-                <Grid item xs={5} marginLeft={4}>
-                  <Box bgcolor="#FF5C35" marginTop={12}>
-                    <Typography variant="h6" color="white" marginLeft={2}>
-                      Featured
-                    </Typography>
-                    <Typography
-                      variant="h4"
-                      color="white"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      {row.eventName}
-                    </Typography>
-                    <Typography
-                      variant="subtitle2"
-                      justifyItems="center"
-                      color="white"
-                    >
-                      {row.eventDescription}
-                    </Typography>
-                    <Box marginTop={2} marginLeft={2}>
-                      <Button
-                        variant="outlined"
-                         href={`/EventDetails/${row.eventId}`}
-                        sx={{ color: "white", borderColor: "white" }}
-                      >
-                        Get tickets
-                      </Button>
-                    </Box>
-                  </Box>
-                </Grid>
-              </Grid>
+    const bearSlideItemData: TBearSlideItemDataList = featured.map((row: any) => {
+      return {
+        key: row.id,
+        children: (
+          <BearSlideCard>
+            <div style={{
+              position: 'relative',
+              width: '100%',
+              height: '400px',
+            }}>
+              <img
+                src={`https://authenticket.s3.ap-southeast-1.amazonaws.com/event_images/${row.eventImage}`}
+                style={{
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
+                  maxWidth: '100%',
+                  maxHeight: '100%',
+                }}
+              />
+            </div>
+          </BearSlideCard>
 
-  
-            </Box>
-          ),
-        };
-      }
-    );
+        ),
+      };
+    });
     return (
       <BearCarousel
         data={bearSlideItemData}
         height="400px"
-        isEnableAutoPlay
-        isEnablePagination
-        isEnableNavButton
         isEnableLoop
+        isEnableAutoPlay
+      />
+    );
+  };
+
+  const TextAnimationsCarousel = () => {
+    const slideItemData: TBearSlideItemDataList = featured.map((row: any) => {
+      return {
+        key: row.featuredId,
+        children: (
+          <Box bgcolor="#FF5C35" marginTop={8}>
+            <Typography variant="h6" color="white" marginLeft={2}>
+              Featured
+            </Typography>
+            <Typography variant="h4" color="white" sx={{ fontWeight: "bold" }}>
+              {row.eventName}
+            </Typography>
+            <Typography variant="subtitle2" justifyItems="center" color="white">
+              {row.eventDescription}
+            </Typography>
+            <Box marginTop={2} marginLeft={2}>
+              <Button
+                variant="outlined"
+                href={`/EventDetails/${row.eventId}`}
+                sx={{ color: "white", borderColor: "white" }}
+              >
+                Get tickets
+              </Button>
+            </Box>
+          </Box>
+        ),
+      };
+    });
+    return (
+      <BearCarousel
+        data={slideItemData}
+        height="400px"
+        isEnableAutoPlay
+        isEnableLoop
+
       />
     );
   };
@@ -330,56 +318,51 @@ export const Home = () => {
         <Typography>
           {props.eventName}
         </Typography>
-
       </Box>
-    );
+    )
   }
 
   function BestSellersCarousell() {
-    return (
-      <Carousel
-        additionalTransfrom={0}
-        arrows
-        autoPlaySpeed={3000}
-        centerMode={false}
-        className=""
-        containerClass="container"
-        dotListClass=""
-        draggable
-        focusOnSelect={false}
-        infinite
-        itemClass=""
-        keyBoardControl
-        minimumTouchDrag={80}
-        partialVisible
-        pauseOnHover
-        renderArrowsWhenDisabled={false}
-        renderButtonGroupOutside={false}
-        renderDotsOutside={false}
-        responsive={{
-          desktop: {
-            breakpoint: {
-              max: 3000,
-              min: 1024,
-            },
-            items: 3,
-            partialVisibilityGutter: 20,
+    return <Carousel
+      additionalTransfrom={0}
+      arrows
+      autoPlaySpeed={3000}
+      centerMode={false}
+      className=""
+      containerClass="container"
+      dotListClass=""
+      draggable
+      focusOnSelect={false}
+      infinite
+      itemClass=""
+      keyBoardControl
+      minimumTouchDrag={80}
+      partialVisible
+      pauseOnHover
+      renderArrowsWhenDisabled={false}
+      renderButtonGroupOutside={false}
+      renderDotsOutside={false}
+      responsive={{
+        desktop: {
+          breakpoint: {
+            max: 3000,
+            min: 1024,
           },
-          mobile: {
-            breakpoint: {
-              max: 464,
-              min: 0,
-            },
-            items: 1,
-            partialVisibilityGutter: 30,
+          items: 3,
+          partialVisibilityGutter: 20,
+        },
+        mobile: {
+          breakpoint: {
+            max: 464,
+            min: 0,
           },
-          tablet: {
-            breakpoint: {
-              max: 1024,
-              min: 464,
-            },
-            items: 2,
-            partialVisibilityGutter: 30,
+          items: 1,
+          partialVisibilityGutter: 30,
+        },
+        tablet: {
+          breakpoint: {
+            max: 1024,
+            min: 464,
           },
           items: 2,
           partialVisibilityGutter: 30,
@@ -402,57 +385,50 @@ export const Home = () => {
         />
       ))}
     </Carousel>;
-
   }
 
   function RecentCarousell() {
-    return (
-      <Carousel
-        additionalTransfrom={0}
-        arrows
-        autoPlaySpeed={3000}
-        centerMode={false}
-        className=""
-        containerClass="container"
-        dotListClass=""
-        draggable
-        focusOnSelect={false}
-        infinite
-        itemClass=""
-        keyBoardControl
-        minimumTouchDrag={80}
-        partialVisible
-        pauseOnHover
-        renderArrowsWhenDisabled={false}
-        renderButtonGroupOutside={false}
-        renderDotsOutside={false}
-        responsive={{
-          desktop: {
-            breakpoint: {
-              max: 3000,
-              min: 1024,
-            },
-            items: 3,
-            partialVisibilityGutter: 20,
+    return <Carousel
+      additionalTransfrom={0}
+      arrows
+      autoPlaySpeed={3000}
+      centerMode={false}
+      className=""
+      containerClass="container"
+      dotListClass=""
+      draggable
+      focusOnSelect={false}
+      infinite
+      itemClass=""
+      keyBoardControl
+      minimumTouchDrag={80}
+      partialVisible
+      pauseOnHover
+      renderArrowsWhenDisabled={false}
+      renderButtonGroupOutside={false}
+      renderDotsOutside={false}
+      responsive={{
+        desktop: {
+          breakpoint: {
+            max: 3000,
+            min: 1024,
           },
-          mobile: {
-            breakpoint: {
-              max: 464,
-              min: 0,
-            },
-            items: 1,
-            partialVisibilityGutter: 30,
+          items: 3,
+          partialVisibilityGutter: 20,
+        },
+        mobile: {
+          breakpoint: {
+            max: 464,
+            min: 0,
           },
-          tablet: {
-            breakpoint: {
-              max: 1024,
-              min: 464,
-            },
-            items: 2,
-            partialVisibilityGutter: 30,
+          items: 1,
+          partialVisibilityGutter: 30,
+        },
+        tablet: {
+          breakpoint: {
+            max: 1024,
+            min: 464,
           },
-
-
           items: 2,
           partialVisibilityGutter: 30,
         },
@@ -474,54 +450,49 @@ export const Home = () => {
         />
       ))}
     </Carousel>;
-
   }
 
   function UpcomingCarousell() {
-    return (
-      <Carousel
-        additionalTransfrom={0}
-        arrows
-        autoPlaySpeed={3000}
-        centerMode={false}
-        className=""
-        containerClass="container"
-        dotListClass=""
-        draggable
-        focusOnSelect={false}
-        infinite
-        itemClass=""
-        keyBoardControl
-        minimumTouchDrag={80}
-        partialVisible
-        pauseOnHover
-        renderArrowsWhenDisabled={false}
-        renderButtonGroupOutside={false}
-        renderDotsOutside={false}
-        responsive={{
-          desktop: {
-            breakpoint: {
-              max: 3000,
-              min: 1024,
-            },
-            items: 3,
-            partialVisibilityGutter: 20,
+    return <Carousel
+      additionalTransfrom={0}
+      arrows
+      autoPlaySpeed={3000}
+      centerMode={false}
+      className=""
+      containerClass="container"
+      dotListClass=""
+      draggable
+      focusOnSelect={false}
+      infinite
+      itemClass=""
+      keyBoardControl
+      minimumTouchDrag={80}
+      partialVisible
+      pauseOnHover
+      renderArrowsWhenDisabled={false}
+      renderButtonGroupOutside={false}
+      renderDotsOutside={false}
+      responsive={{
+        desktop: {
+          breakpoint: {
+            max: 3000,
+            min: 1024,
           },
-          mobile: {
-            breakpoint: {
-              max: 464,
-              min: 0,
-            },
-            items: 1,
-            partialVisibilityGutter: 30,
+          items: 3,
+          partialVisibilityGutter: 20,
+        },
+        mobile: {
+          breakpoint: {
+            max: 464,
+            min: 0,
           },
-          tablet: {
-            breakpoint: {
-              max: 1024,
-              min: 464,
-            },
-            items: 2,
-            partialVisibilityGutter: 30,
+          items: 1,
+          partialVisibilityGutter: 30,
+        },
+        tablet: {
+          breakpoint: {
+            max: 1024,
+            min: 464,
           },
           items: 2,
           partialVisibilityGutter: 30,
@@ -546,9 +517,12 @@ export const Home = () => {
     </Carousel>;
   }
 
+
+
+
   return (
     <>
-      {loaded ? (
+      {loaded ?
         <Box>
           <div>
             {token != null ? <NavbarLoggedIn /> : <NavbarNotLoggedIn />}
@@ -581,6 +555,7 @@ export const Home = () => {
                 alignItems="center"
                 justifyContent="center"
               >
+
                 <Grid item md={6}>
                   <Box
                     sx={{
@@ -645,8 +620,15 @@ export const Home = () => {
               <RecentCarousell />
             </Grid>
           </Grid>
-          <Box bgcolor="#FF5C35" marginTop={12} >
-            <CustomBanner></CustomBanner>
+          <Box bgcolor="#FF5C35" marginTop={12}>
+            <Grid container justifyContent="center" alignItems="center">
+              <Grid item xs={5} marginTop={4} marginBottom={4} >
+                <CustomBanner />
+              </Grid>
+              <Grid item xs={5} marginLeft={4}>
+                <TextAnimationsCarousel />
+              </Grid>
+            </Grid>
           </Box>
           <Typography marginLeft={10} marginTop={8} sx={{ fontWeight: "bold" }}>
             Recently Added
@@ -657,7 +639,8 @@ export const Home = () => {
             </Grid>
           </Grid>
         </Box>
-      ) : null}
+        : null}
+
     </>
   );
 };
