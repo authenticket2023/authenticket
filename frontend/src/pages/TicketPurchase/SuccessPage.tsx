b9036909f70c09c2a2f9350566cf2d70cc76b07e
xXxPMWxXx <panmingwei123@gmail.com>
2023-11-06 13:01:05 +0800

    resolve conflict

--- frontend/src/pages/TicketPurchase/SuccessPage.tsx
+++ frontend/src/pages/TicketPurchase/SuccessPage.tsx
@@ -60,29 +60,15 @@ function formatDate(dateString: string): string {
 
 export const SuccessPage: React.FC = (): JSX.Element => {
   // Set parameters
+  const token = window.localStorage.getItem('accessToken');
   const { orderId } = useParams<{ orderId: string }>();
-  //   const { enteredData } = useParams<EnteredData>();
-
-
-  type Params = 'images' | 'names' | 'sectionID' | 'row' | 'seat';
-  const params = useParams<Params>();
-  const images = params.images ? JSON.parse(params.images) as File[] : [];
-  const names = params.names ? JSON.parse(params.names) as string[] : [];
-  const sectionID = params.sectionID;
-  const row = params.row;
-  const seat = params.seat;
-
-
+  const [summaryLoaded, setLoaded]: any = useState(false)
+  const [orderSummary, setOrderSummary] : any = useState<OrderSummary | undefined>();
 
   useEffect(() => {
     completeOrder(orderId);
     orderInfo(orderId);
   }, []);
-
-  const token = window.localStorage.getItem('accessToken');
-  const [summaryLoaded, setLoaded]: any = useState(false)
-  const [orderSummary, setOrderSummary] : any = useState<OrderSummary | undefined>();
-
 
   // Call backend to complete order
   const completeOrder = async (orderId: any) => {
@@ -94,22 +80,6 @@ export const SuccessPage: React.FC = (): JSX.Element => {
       },
       method: 'PUT',
     })
-      .then(async (response) => {
-        if (response.status === 200) {
-          console.log('Order completed');
-          const apiResponse = await response.json();
-          const data = apiResponse.data;
-          console.log(apiResponse)
-          setCompletedOrder(data.event);
-
-          //only do submitFace if event is enhanced
-          if (completedOrder?.isEnhanced) {
-            submitFace();
-          }
-        } else {
-          // Handle error or pass to parent component
-        }
-      })
       .catch((err) => {
         window.alert(err);
       });
@@ -137,60 +107,6 @@ export const SuccessPage: React.FC = (): JSX.Element => {
       .catch((err) => {
         window.alert(err);
       }); 
-  };
-
-  // Call backend API for face
-  const submitFace = async () => {
-    console.log("face")
-    // Create order
-    const formData = new FormData();
-    formData.append('eventID', String(completedOrder?.eventId));
-
-    // Check if orderSummary and its ticketSet are defined
-    if (orderSummary && orderSummary.ticketSet) {
-      // Iterate through ticketSet
-      orderSummary.ticketSet.forEach((ticket, index) => {
-        const { sectionId, rowNo, seatNo } = ticket;
-
-        // Check if corresponding image and name exist
-        if (images[index] && names[index]) {
-          // Append the image using 'image' + (index + 1) as the key
-          formData.append(`image${index + 1}`, images[index]);
-
-          // Append the info using 'info' + (index + 1) as the key
-          const info = `${names[index]},${sectionId},${rowNo},${seatNo}`;
-          formData.append(`info${index + 1}`, info);
-        }
-
-        //add in email for facial api to verify the token
-        formData.append('email', currUserEmail);
-
-        //call backend
-        fetch(`${process.env.REACT_APP_FACIAL_URL}/face/facial-creation`, {
-          headers: {
-            'Authorization': `Bearer ${token}`,
-          },
-          method: 'POST',
-          body: formData
-        })
-          .then(async (response) => {
-            if (response.status == 200 || response.status == 201) {
-              // Parse the JSON response
-              const responseData = await response.json();
-              // Access the orderId from the response data
-              const orderId = responseData.data.orderId;
-
-            } else {
-
-            }
-          })
-          .catch((err) => {
-            window.alert(err);
-          })
-
-      });
-    }
-
   };
 
   return (
