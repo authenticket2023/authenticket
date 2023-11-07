50b2a12e3755c273857c7c93fc3866f608f3d6e7
ChuaYiKai <142324521+ChuaYiKai@users.noreply.github.com>
2023-11-07 20:21:29 +0800

    AdminControllerIntegration test

--- backend/src/test/java/com/authenticket/authenticket/controller/AdminControllerIntegrationTest.java
+++ backend/src/test/java/com/authenticket/authenticket/controller/AdminControllerIntegrationTest.java
@@ -1,18 +1,14 @@
 package com.authenticket.authenticket.controller;
 
-import com.authenticket.authenticket.model.EventOrganiser;
-import com.authenticket.authenticket.model.User;
+import com.authenticket.authenticket.model.*;
 import com.authenticket.authenticket.repository.*;
 import com.authenticket.authenticket.service.AdminService;
 import com.authenticket.authenticket.service.JwtService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.authenticket.authenticket.model.Admin;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -23,13 +19,13 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AdminControllerIntegrationTest {
 
@@ -49,6 +45,18 @@ public class AdminControllerIntegrationTest {
     EventOrganiserRepository eventOrganiserRepository;
 
     @Autowired
+    VenueRepository venueRepository;
+
+    @Autowired
+    ArtistRepository artistRepository;
+
+    @Autowired
+    EventRepository eventRepository;
+
+    @Autowired
+    EventTypeRepository eventTypeRepository;
+
+    @Autowired
     JwtService jwtService;
 
     private static HttpHeaders headers;
@@ -60,19 +68,169 @@ public class AdminControllerIntegrationTest {
 
     String token;
 
-    @BeforeEach
-    public void setUp() {
+    @BeforeAll
+    public void init() {
         objectMapper.registerModule(new JavaTimeModule());
         Optional<Admin> oldAdmin = adminRepository.findByEmail("admin@gmail.com");
-        if (!oldAdmin.isEmpty()) {
+        if (oldAdmin.isPresent()) {
             adminRepository.delete(oldAdmin.get());
         }
         String encodedPassword = new BCryptPasswordEncoder().encode("password");
+
         // ID is auto set to 1 when saving so changing this adminId is pointless
         admin = Admin.builder().adminId(1).password(encodedPassword).email("admin@gmail.com").name("admin").build();
         adminRepository.saveAndFlush(admin);
         // ID is auto set to 2 when saving so changing this adminId is pointless
         anotherAdmin = Admin.builder().adminId(2).password(encodedPassword).email("admin2@gmail.com").name("admin2").build();
+        adminRepository.saveAndFlush(anotherAdmin);
+
+        Artist artist = Artist.builder()
+                .artistId(1)
+                .artistName("GeorgiaTest")
+                .artistImage("null")
+                .events(null)
+                .build();
+        Set<Artist> artists = new HashSet<>();
+        artists.add(artist);
+        artistRepository.saveAndFlush(artist);
+
+        Venue venue = Venue.builder()
+                .venueId(1)
+                .venueName("testPlace")
+                .venueLocation("testPlaceLocation")
+                .venueImage(null)
+                .build();
+        venueRepository.saveAndFlush(venue);
+        EventOrganiser eventOrganiser = EventOrganiser
+                .builder()
+                .organiserId(1)
+                .name("TestGeorgia")
+                .email("testOrg1@example.com")
+                .password("password")
+                .description("description")
+                .logoImage(null)
+                .enabled(true)
+                .admin(null)
+                .reviewStatus("approved")
+                .reviewRemarks("remarks")
+                .events(new ArrayList<Event>())
+                .build();
+        eventOrganiserRepository.saveAndFlush(eventOrganiser);
+        EventType eventType = new EventType(1, "newEventType");
+        eventTypeRepository.saveAndFlush(eventType);
+        Event event = Event.builder()
+                .eventId(1)
+                .eventName("Test Event")
+                .eventDescription("A test event description.")
+                .eventDate(LocalDateTime.now())
+                .otherEventInfo("Additional event info.")
+                .eventImage("event-image.jpg")
+                .ticketSaleDate(LocalDateTime.now().plusDays(7))
+                .reviewedBy(admin)
+                .reviewStatus(Event.ReviewStatus.PENDING.getStatusValue())
+                .reviewRemarks("Review remarks")
+                .isEnhanced(true)
+                .hasPresale(true)
+                .hasPresaleUsers(true)
+                .organiser(eventOrganiser)
+                .venue(venue)
+                .eventType(eventType)
+                .artists(artists)
+                .build();
+        eventRepository.saveAndFlush(event);
+
+        EventOrganiser organiser2 = EventOrganiser
+                .builder()
+                .organiserId(2)
+                .name("TestGeorgia")
+                .email("testOrg2@example.com")
+                .password("password")
+                .description("description")
+                .logoImage(null)
+                .enabled(true)
+                .admin(null)
+                .reviewStatus("approved")
+                .reviewRemarks("remarks")
+                .events(new ArrayList<Event>())
+                .build();
+        eventOrganiserRepository.saveAndFlush(organiser2);
+
+        EventOrganiser organiser3 = EventOrganiser
+                .builder()
+                .organiserId(3)
+                .name("TestGeorgia")
+                .email("testOrg3@example.com")
+                .password("password")
+                .description("description")
+                .logoImage(null)
+                .enabled(true)
+                .admin(null)
+                .reviewStatus("pending")
+                .reviewRemarks("remarks")
+                .events(new ArrayList<Event>())
+                .build();
+        eventOrganiserRepository.saveAndFlush(organiser3);
+
+        Event event1 = Event.builder()
+                .eventId(2)
+                .eventName("Test Event1")
+                .eventDescription("A test event description.")
+                .eventDate(LocalDateTime.now())
+                .otherEventInfo("Additional event info.")
+                .eventImage("event-image.jpg")
+                .ticketSaleDate(LocalDateTime.now().plusDays(7))
+                .reviewedBy(admin)
+                .reviewStatus(Event.ReviewStatus.APPROVED.getStatusValue())
+                .reviewRemarks("Review remarks")
+                .isEnhanced(true)
+                .hasPresale(true)
+                .hasPresaleUsers(true)
+                .organiser(eventOrganiser)
+                .venue(venue)
+                .eventType(eventType)
+                .artists(artists)
+                .build();
+        eventRepository.saveAndFlush(event1);
+        Event event2 = Event.builder()
+                .eventId(3)
+                .eventName("Test Event2")
+                .eventDescription("A test event description.")
+                .eventDate(LocalDateTime.now())
+                .otherEventInfo("Additional event info.")
+                .eventImage("event-image.jpg")
+                .ticketSaleDate(LocalDateTime.now().plusDays(7))
+                .reviewedBy(admin)
+                .reviewStatus(Event.ReviewStatus.APPROVED.getStatusValue())
+                .reviewRemarks("Review remarks")
+                .isEnhanced(true)
+                .hasPresale(true)
+                .hasPresaleUsers(true)
+                .organiser(eventOrganiser)
+                .venue(venue)
+                .eventType(eventType)
+                .artists(artists)
+                .build();
+        eventRepository.saveAndFlush(event2);
+        Event event3 = Event.builder()
+                .eventId(4)
+                .eventName("Test Event3")
+                .eventDescription("A test event description.")
+                .eventDate(LocalDateTime.now())
+                .otherEventInfo("Additional event info.")
+                .eventImage("event-image.jpg")
+                .ticketSaleDate(LocalDateTime.now().plusDays(7))
+                .reviewedBy(admin)
+                .reviewStatus(Event.ReviewStatus.PENDING.getStatusValue())
+                .reviewRemarks("Review remarks")
+                .isEnhanced(true)
+                .hasPresale(true)
+                .hasPresaleUsers(true)
+                .organiser(eventOrganiser)
+                .venue(venue)
+                .eventType(eventType)
+                .artists(artists)
+                .build();
+        eventRepository.saveAndFlush(event3);
 
         token = jwtService.generateUserToken(admin);
         headers = new HttpHeaders();
@@ -80,8 +238,13 @@ public class AdminControllerIntegrationTest {
         headers.add("Authorization", "Bearer " + token);
     }
 
-    @AfterEach
-    void tearDown() {
+    @AfterAll
+    void deleteAdmin(){
+        artistRepository.deleteAll();
+        venueRepository.deleteAll();
+        eventRepository.deleteAll();
+        eventOrganiserRepository.deleteAll();
+        eventTypeRepository.deleteAll();
         adminRepository.deleteAll();
     }
 
@@ -91,7 +254,6 @@ public class AdminControllerIntegrationTest {
 
     @Test
     public void testFindAllAdmin() {
-        adminRepository.saveAndFlush(anotherAdmin);
         HttpEntity<String> entity = new HttpEntity<>(null, headers);
         ResponseEntity<String> responseEntity = restTemplate.exchange(createURLWithPort(), HttpMethod.GET, entity, String.class);
 
@@ -103,12 +265,9 @@ public class AdminControllerIntegrationTest {
     @Test
     public void testFindAdminById() {
         HttpEntity<String> entity = new HttpEntity<>(null, headers);
-        // Send a GET request to the "/api/v2/admin/{admin_id}" endpoint
-        ResponseEntity<String> responseEntity = restTemplate.exchange(createURLWithPort() + "/1", HttpMethod.GET, entity, String.class);
-        // Check if the response status code is 200 (OK) for a valid admin ID
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
 
-        // You can also check the response body or other aspects of the response
+        ResponseEntity<String> responseEntity = restTemplate.exchange(createURLWithPort() + "/1", HttpMethod.GET, entity, String.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntity.getBody()).contains("admin@gmail.com");
     }
 
@@ -118,7 +277,7 @@ public class AdminControllerIntegrationTest {
         HttpEntity<String> entity = new HttpEntity<>(objectMapper.writeValueAsString(admin), headers);
 
         ResponseEntity<String> responseEntity = restTemplate.exchange(createURLWithPort(), HttpMethod.PUT, entity, String.class);
-        System.out.println(responseEntity.getBody());
+
         // Check if the response status code is 200 (OK) for a valid update
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
 
@@ -128,72 +287,52 @@ public class AdminControllerIntegrationTest {
 
     @Test
     public void testUpdateEventOrganiser() throws JsonProcessingException {
-        // Implement a test for the 'update-organiser' endpoint
-        // Send a PUT request with the necessary parameters and check the response
-
-        // Example:
-        // - Create a new EventOrganiserUpdateDto with some changes
-        // - Send a PUT request with the changes to the 'update-organiser' endpoint
-        // - Check if the response status code is as expected (e.g., HttpStatus.OK)
-        // - Check if the response body contains the expected data
-        EventOrganiser organiser = new EventOrganiser();
-        organiser.setDescription("desc");
-        organiser.setPassword("bla");
-        organiser.setEmail("e");
-        organiser.setName("E");
-        eventOrganiserRepository.saveAndFlush(organiser);
-        MultiValueMap<String, String> body = new LinkedMultiValueMap<String, String>();
-        body.add("organiserId", "1");
-        body.add("name", "new name");
-
-        HttpEntity<?> entity = new HttpEntity<>(body, headers);
+        HttpEntity<?> entity = new HttpEntity<>(null, headers);
 
         ResponseEntity<String> responseEntity = restTemplate.exchange(createURLWithPort() + "/update-organiser?organiserId=1&name=new name", HttpMethod.PUT, entity, String.class);
-        System.out.println(responseEntity.getBody());
+
         // Check if the response status code is 200 (OK) for a valid update
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-        // Check if the response contains the updated admin's name
+        assertThat(responseEntity.getBody()).contains("Event organiser 1 updated successfully.");
         assertThat(responseEntity.getBody()).contains("new name");
     }
 
     @Test
     public void testUpdateEvent() {
-        // Implement a test for the 'update-event' endpoint
-        // Send a PUT request with the necessary parameters and check the response
 
-        // Example:
-        // - Create a new EventUpdateDto with some changes
-        // - Send a PUT request with the changes to the 'update-event' endpoint
-        // - Check if the response status code is as expected (e.g., HttpStatus.OK)
-        // - Check if the response body contains the expected data
+        HttpEntity<?> entity = new HttpEntity<>(null, headers);
 
-        // You can use a similar approach as in the 'testUpdateAdmin' method.
+        ResponseEntity<String> responseEntity = restTemplate.exchange(createURLWithPort() + "/update-event?eventId=1&eventName=new name", HttpMethod.PUT, entity, String.class);
+
+        // Check if the response status code is 200 (OK) for a valid update
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).contains("new name");
     }
 
     @Test
     public void testFindEventOrganisersByReviewStatus() {
-        // Implement a test for the 'event-organiser/review-status/{status}' endpoint
-        // Send a GET request with a valid status (e.g., "approved") and check the response
+        HttpEntity<?> entity = new HttpEntity<>(null, headers);
 
-        // Example:
-        // - Send a GET request to the 'event-organiser/review-status/approved' endpoint
-        // - Check if the response status code is as expected (e.g., HttpStatus.OK)
-        // - Check if the response body contains data for approved event organizers
+        ResponseEntity<String> responseEntity = restTemplate.exchange(createURLWithPort() + "/event-organiser/review-status/approved", HttpMethod.GET, entity, String.class);
+        System.out.println(responseEntity.getBody());
+        // Check if the response status code is 200 (OK) for a valid update
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).contains("testOrg1@example.com");
+        assertThat(responseEntity.getBody()).contains("testOrg2@example.com");
+        assertThat(responseEntity.getBody()).doesNotContain("testOrg3@example.com");
 
-        // You can use a similar approach as in the 'testFindAllAdmin' method.
     }
 
     @Test
     public void testFindEventsByReviewStatus() {
-        // Implement a test for the 'event/review-status/{status}' endpoint
-        // Send a GET request with a valid status (e.g., "approved") and check the response
+        HttpEntity<?> entity = new HttpEntity<>(null, headers);
 
-        // Example:
-        // - Send a GET request to the 'event/review-status/approved' endpoint
-        // - Check if the response status code is as expected (e.g., HttpStatus.OK)
-        // - Check if the response body contains data for approved events
+        ResponseEntity<String> responseEntity = restTemplate.exchange(createURLWithPort() + "/event/review-status/approved", HttpMethod.GET, entity, String.class);
 
-        // You can use a similar approach as in the 'testFindAllAdmin' method.
+        // Check if the response status code is 200 (OK) for a valid update
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).contains("Test Event1");
+        assertThat(responseEntity.getBody()).contains("Test Event2");
+        assertThat(responseEntity.getBody()).doesNotContain("Test Event3");
     }
 }
